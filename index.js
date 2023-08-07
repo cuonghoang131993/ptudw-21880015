14150b57ce1f3d61c721704245c5a33a066e0c8f
Cuong Hoang <cuonghoang131993@gmail.com>
2023-08-07 21:55:06 +0700

    sequelize create table Products, Images, Brands

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.engine('hbs', expressHandlerbars.engine({
 app.set('view engine', 'hbs');
 
 // routes
+app.get('/createTables', (req, res) => {
+  let models = require('./models');
+  models.sequelize.sync().then(() => {
+    res.send('tables created');
+  });
+});
+
 app.get('/', (req, res) => {
     res.render('index');
 });
